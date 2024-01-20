cf7abe41b72828d53ce5548a46998daf1a8081af
mynkprtp <itsmayankhere24@gmail.com>
2024-01-20 15:56:32 +0530

    Vercel Insights

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,10 +33,10 @@ export default function RootLayout({
             <Header />
             {children}
             <Footer />
-
             <Toaster position="top-right" />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
